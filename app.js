cd59df1c7607aab8ecac72b5ccaa2a5ada5da63b
Cecilia Giorcelli <giorcelceci@gmail.com>
2017-04-28 19:41:11 -0400

    Test

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var router = express.Router() // get an instance of the express Router
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-	// do logging
+	// do logging.
 	console.log('Something is happening')
 	next() // make sure we go to the next routes and don't stop here
 })
